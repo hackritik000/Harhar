0c6a9d827b8de7f137cef2a20e5842a64e5e22c2
hackritik000 <hackritik000@gmail.com>
2024-09-04 16:28:54 +0530

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: ".",
+      },
+    },
+  },
+  {
+    ignores: ["**/*.d.ts"],
+  },
+  {
+    rules: {
+      "no-ignore": "off",
+    },
+  }
+);
