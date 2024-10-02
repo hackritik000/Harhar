396f6a9989f65f7bf5261980ee78cc5f2043625d
hackritik000 <hackritik000@gmail.com>
2024-10-02 11:30:42 +0530

    Stop trancking .astro

--- .astro/astro/actions.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "astro:actions" {
-	type Actions = typeof import("D:/New_folder/Harhar/src/actions")["server"];
-
-	export const actions: Actions;
-}
--- .astro/types.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="astro/client" />
-/// <reference path="astro/actions.d.ts" />
