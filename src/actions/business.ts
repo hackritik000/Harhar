cde137453981099e3dba3a3f7f592ac57fc2a014
karan-gander <meghawalkaran@gmail.com>
2024-10-04 12:42:14 +0530

    for sloving error in accept

--- src/actions/business.ts
+++ src/actions/business.ts
@@ -1,8 +1,10 @@
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { businessDetails } from "@/schema/bussness.schema";
 
 import { db } from "@/lib/db";
+import { TooManyRequest } from "@/utils/tooManyRequest";
+import { eq } from "drizzle-orm";
 export const business = {
   addBusiness: defineAction({
     accept: "form",
@@ -27,12 +29,29 @@ export const business = {
       businessDescription: z.string(),
     }),
     handler: async (input, ctx) => {
+      if (TooManyRequest(ctx)) {
+        throw new ActionError({
+          code: 'TOO_MANY_REQUESTS',
+        });
+      }
       console.log(input);
       const listing = await db.insert(businessDetails).values(input);
-      
+
     },
   }),
-  showListing:defineAction(
+  showListing: defineAction({
+    accept: "json",
 
-  )
+    handler: async (input, ctx) => {
+      if (TooManyRequest(ctx)) {
+        throw new ActionError({
+          code: 'TOO_MANY_REQUESTS',
+        });
+      }
+
+      const listingDetails = await db.select().from(businessDetails).where(eq(businessDetails.id,input.listingId))
+      console.log(listingDetails)
+
+    }
+  })
 };
